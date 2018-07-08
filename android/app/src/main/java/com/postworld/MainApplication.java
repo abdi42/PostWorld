53fe756fd4ac584ec5552f9908f6774b3429b4d5
abdullahi <abdullahimahamed0987@gmail.com>
2018-07-08 13:58:34 -0500

    updated ios and android build folders

--- android/app/src/main/java/com/postworld/MainApplication.java
+++ android/app/src/main/java/com/postworld/MainApplication.java
@@ -3,6 +3,10 @@ package com.postworld;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
+import com.horcrux.svg.SvgPackage;
+import com.mapbox.reactnativemapboxgl.ReactNativeMapboxGLPackage;
+import com.mapbox.rctmgl.RCTMGLPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +28,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
+            new SvgPackage(),
+            new ReactNativeMapboxGLPackage(),
+            new RCTMGLPackage(),
             new VectorIconsPackage()
       );
     }
